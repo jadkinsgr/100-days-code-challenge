050472894dfe8d488df9697813667bc61d9bc4ec
Josh Adkins <joshua.adkins@healthcatalyst.com>
2022-03-18 10:27:22 -0400

    updates

--- filecreator.py
+++ filecreator.py
@@ -1,9 +1,9 @@
 import os
 
-num = 11 
+num = 10
 while num < 100:
     pdir = '/Users/joshua.adkins/Github/100-days-code-challenge/'
     num += 1
-    com = pdir+'Day '+str(num)
+    com = pdir+'Day'+str(num)
     os.mkdir(com)
     #print(com)
