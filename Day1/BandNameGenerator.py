1d3566fc6096af1bca22b80b45fc8baf2e5622a1
Josh Adkins <joshua.adkins@healthcatalyst.com>
2022-04-18 09:20:23 -0400

    Updates to day 1 - final project

--- Day1/BandNameGenerator.py
+++ Day1/BandNameGenerator.py
@@ -1,5 +1,4 @@
-print('Welcome to the Band Name Generator.')
+print('Welcome to the Band Name Generator. This is helpful by creating your band name!')
 city = input('What is the city you grew up in?\n')
 pet = input('What is the name of your pet?\n')
-
 print("Your band name could be: \nThe " + city + " " + pet+".")
