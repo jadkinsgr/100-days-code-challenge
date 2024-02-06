c319bac9dbdd7c71da400c7125a605af7b46f4ca
Josh Adkins <jadkinsgr@gmail.com>
2024-02-06 15:31:26 -0500

    Day2 updates

--- Day2/TipCalculator.py
+++ Day2/TipCalculator.py
@@ -1,16 +1,20 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percent tip would you like to give? "))
-people = int(input("How many people to split the bill?"))
+#If the bill was $150.00, split between 5 people, with 12% tip. 
 
-tip_as_percent = tip / 100
-total_tip_amount = bill * tip_as_percent
-total_bill = bill + total_tip_amount
-bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
 
-#FAQ: How to round to 2 decimal places?
-#https://www.udemy.com/course/100-days-of-code/learn/lecture/17965132#questions/13315048
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
 
-print(
-    f"The total bill was ${bill}, with a tip of {tip} percent the new total is ${total_bill}, each person should pay: ${final_amount}")
+#Write your code below this line 👇
+totalbill=float(input("What is the total Bill?"))
+people=int(input("How many people are there?"))
+tip_percentage=(float(input("% of the bill added as tip?")))/100
+
+# print(totalbill)
+# print(people)   
+# print(tip_percentage)
+
+totalPerPerson =  (totalbill/people) * (1+tip_percentage)
+totalPerPerson = round(totalPerPerson, 2)
+
+print(f"If the bill was {totalbill}, split between {people} people, with a {tip_percentage*100}% tip. Each person should pay ${totalPerPerson}.")
